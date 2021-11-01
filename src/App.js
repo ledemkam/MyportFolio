eb9a84265ca55e2813ca82e09dddd652a2193a62
ledemkam <81742990+ledemkam@users.noreply.github.com>
2021-11-01 21:43:33 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Home from './pages/Home';
 import Contact from './pages/Contact';
 import Knowledges from './pages/Knowledges';
-import Notfound from './pages/NotFound';
+
 import Portfolio from './pages/Portfolio';
 
 const App = () => {
@@ -15,7 +15,7 @@ const App = () => {
                <Route path="/Fähigkeiten" component={Knowledges} />
                <Route path="/portfolio" component={Portfolio} />
                <Route path="/Kontakt" component={Contact} />
-               <Route component={Notfound} />
+              
             </Switch>
          </BrowserRouter>
       </>
